83ea180686f89f6e8a77adef3cafba3462cc951d
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-17 00:34:21 +0300

    test case 6

--- _printf.c
+++ _printf.c
@@ -15,10 +15,7 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	if (format == NULL)
-		return (-1);
-
-	if ((format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	current = format;
