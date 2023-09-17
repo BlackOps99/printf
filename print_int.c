e6d34d769b94084ec08754635fb1c7a3ea03fd73
Sarah <sarahaku12@gmail.com>
2023-09-17 23:52:43 +0300

    edit

--- print_int.c
+++ print_int.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-int print_number(unsigned int number);
-int count_digits(unsigned int num);
-int extract_digit(unsigned int num, int index);
-
 /**
  * toInt - Print an integer as a string
  * @value: The integer
@@ -12,82 +8,45 @@ int extract_digit(unsigned int num, int index);
  */
 int toInt(va_list value)
 {
-	int n = va_arg(value, int);
-	unsigned int un_n;
+    int n = va_arg(value, int);
+    int num, digit, count = 0;
+    unsigned int un_n;
 
-	if (n < 0)
-	{
-		un_n = -n;
-		_putchar('-');
-	}
-	else
-	{
-		un_n = n;
-	}
+    if (n < 0)
+    {
+        un_n = -n;
+        _putchar('-');
+        count++;
+    }
+    else
+    {
+        un_n = n;
+    }
 
-	if (un_n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
+    if (un_n == 0)
+    {
+        _putchar('0');
+        count++;
+        return (count);
+    }
 
-	return (print_number(un_n));
-}
+    num = un_n;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
 
-/**
- * print_number - Prints an unsigned integer
- * @number: The unsigned integer
- *
- * Return: The number of digits printed
- */
-int print_number(unsigned int number)
-{
-	int count = count_digits(number);
-	int i = 0;
+    while (count > 0)
+    {
+        digit = un_n;
+        for (int i = 0; i < count - 1; i++)
+        {
+            digit /= 10;
+        }
+        _putchar((digit % 10) + '0');
+        count--;
+    }
 
-	for (i = count; i > 0; i--)
-	{
-		int digit = extract_digit(number, i);
-
-		_putchar(digit + '0');
-	}
-
-	return (count);
-}
-
-/**
- * count_digits - Counts the number of digits in a number
- * @num: The number
- *
- * Return: The number of digits
- */
-int count_digits(unsigned int num)
-{
-	int digits = 0;
-
-	while (num)
-	{
-		digits++;
-		num /= 10;
-	}
-	return (digits);
-}
-
-/**
- * extract_digit - Extracts the desired digit from a number
- * @num: The number
- * @index: The index (from the right, starting with 1)
- *
- * Return: The desired digit
- */
-int extract_digit(unsigned int num, int index)
-{
-	int i = 0;
-
-	for (i = 0; i < index - 1; i++)
-	{
-		num /= 10;
-	}
-
-	return (num % 10);
+    return (un_n < 0 ? count + 1 : count);
 }
