adb7d32c822ea0df37f501c8e5e9bc3a7524c88b
Sarah <sarahaku12@gmail.com>
2023-09-17 22:33:06 +0300

    editing handle_print

--- _printf.c
+++ _printf.c
@@ -1,71 +1,70 @@
 #include "main.h"
 
 int handle_print(const char *to_type, va_list value);
+
 /**
- * _printf - function the print all type of output
- * @format: the format that will be work with it
- * Return: length of format
- */
+* _printf - function that prints all types of output
+* @format: the format that will be worked with
+* Return: length of format
+*/
 int _printf(const char *format, ...)
 {
-	int len_of_str = 0;
-	const char *current;
+int len_of_str = 0;
+const char *current;
 
-	va_list ap;
+va_list ap;
 
-	va_start(ap, format);
+va_start(ap, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
 
-	current = format;
+current = format;
 
-	while (*current != '\0')
-	{
-		if (*current == '%')
-		{
-			current++;
-			if (*current != '\0')
-			{
-				len_of_str += handle_print(current, ap);
-			}
-		}
-		else
-		{
-			_putchar(*current);
-			len_of_str++;
-		}
-		current++;
-	}
-
-	va_end(ap);
-	return (len_of_str);
+while (*current != '\0')
+{
+if (*current == '%')
+{
+current++;
+if (*current != '\0')
+{
+len_of_str += handle_print(current, ap);
 }
+}
+else
+{
+_putchar(*current);
+len_of_str++;
+}
+current++;
+}
+
+va_end(ap);
+return (len_of_str);
+}
+
 /**
- * handle_print - function that handel of type format
- * @to_type: type char pointer
- * @value: type va_list that content the value
- * Return: 0
- */
+* handle_print - function that handles the type format
+* @to_type: type char pointer
+* @value: type va_list that contains the value
+* Return: The number of characters printed.
+*/
 int handle_print(const char *to_type, va_list value)
 {
-	switch (to_type[0])
-	{
-	case 'c':
-	{
-		return (toChar(value));
-	}
-	case 's':
-	{
-		return (toString(value));
-	}
-	case '%':
-	{
-		return (toRatio());
-	}
-	default:
-		_putchar('%');
-		_putchar(*to_type);
-		return (1);
-	}
+switch (to_type[0])
+{
+case 'c':
+return (toChar(value));
+case 's':
+return (toString(value));
+case '%':
+return (toRatio());
+case 'd':
+case 'i':
+return (toInt(value));
+default:
+_putchar('%');
+_putchar(*to_type);
+return (1);
+}
 }
