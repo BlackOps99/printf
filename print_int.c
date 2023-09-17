3372fbbb54e51ae0bd58660a26e9327173c93c96
Sarah <sarahaku12@gmail.com>
2023-09-17 23:58:57 +0300

    edit

--- print_int.c
+++ print_int.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * toInt - Print an integer as a string
- * @value: The integer
- *
- * Return: The number of digits printed
- */
+* toInt - Print an integer as a string
+* @value: The integer
+*
+* Return: The number of digits printed
+*/
 int toInt(va_list value)
 {
-    int n = va_arg(value, int);
-    int num, digit, count = 0;
-    unsigned int un_n;
+int n = va_arg(value, int);
+int num, digit, count = 0;
+unsigned int un_n;
 
-    if (n < 0)
-    {
-        un_n = -n;
-        _putchar('-');
-        count++;
-    }
-    else
-    {
-        un_n = n;
-    }
+if (n < 0)
+{
+un_n = -n;
+_putchar('-');
+count++;
+}
+else
+{
+un_n = n;
+}
 
-    if (un_n == 0)
-    {
-        _putchar('0');
-        count++;
-        return (count);
-    }
+if (un_n == 0)
+{
+_putchar('0');
+count++;
+return (count);
+}
 
-    num = un_n;
-    while (num != 0)
-    {
-        num /= 10;
-        count++;
-    }
+num = un_n;
+while (num != 0)
+{
+num /= 10;
+count++;
+}
 
-    while (count > 0)
-    {
-        digit = un_n;
-        for (int i = 0; i < count - 1; i++)
-        {
-            digit /= 10;
-        }
-        _putchar((digit % 10) + '0');
-        count--;
-    }
+while (count > 0)
+{
+digit = un_n;
+for (int i = 0; i < count - 1; i++)
+{
+digit /= 10;
+}
+_putchar((digit % 10) + '0');
+count--;
+}
 
-    return (un_n < 0 ? count + 1 : count);
+return (un_n < 0 ? count + 1 : count);
 }
