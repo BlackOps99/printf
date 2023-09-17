0ce91a963160733d48ed97c7e5eb10fb3adf08e9
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-17 06:11:02 +0300

    Test Case 15 before sleep

--- _printf.c
+++ _printf.c
@@ -15,7 +15,10 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (!format)
+		return (-1);
+
+	if ((format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	current = format;
