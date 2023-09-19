330147b7c96bc866c37f6cfa1f6ecbae994d915d
Sarah <sarahaku12@gmail.com>
2023-09-19 22:54:52 +0300

    Try New Way

--- print_int.c
+++ print_int.c
@@ -1,7 +1,4 @@
 #include "main.h"
-int print_number(unsigned int number);
-int count_digits(unsigned int num);
-int extract_digit(unsigned int num, int index);
 /**
  * toInt - Print an integer as a string
  * @value: The integer
@@ -11,107 +8,43 @@ int extract_digit(unsigned int num, int index);
 int toInt(va_list value)
 {
 	int n = va_arg(value, int);
-	unsigned int un_n;
+	int i = 0;
 
 	if (n < 0)
 	{
-		un_n = -n;
 		_putchar('-');
+		n = -n;
+		i++;
 	}
-	else
-	{
-		un_n = n;
-	}
-
-	if (un_n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
 
-	return (print_number(un_n));
-}
+	int num_digits = 0;
+	int temp = n;
 
-/**
-* count_digits - Count the digits in a number.
-* @num: The number to count digits for.
-*
-* Return: The number of digits.
-*/
-int count_digits(unsigned int num)
-{
-int digits = 0;
+	while (temp != 0)
+	{
+		temp /= 10;
+		num_digits++;
+	}
 
-while (num)
-{
-digits++;
-num /= 10;
-}
-return (digits);
-}
+	char buffer[12];
 
-/**
-* extract_digit - Extract a specific digit from a number.
-* @num: The number to extract from.
-* @index: The index (from right, starting at 1) of the digit to extract.
-*
-* Return: The desired digit.
-*/
-int extract_digit(unsigned int num, int index)
-{
-int i;
+	buffer[num_digits] = '\0';
 
-for (i = 0; i < index - 1; i++)
-{
-num /= 10;
-}
-return (num % 10);
-}
+	for (int j = num_digits - 1; j >= 0; j--)
+	{
+		buffer[j] = (n % 10) + '0';
+		n /= 10;
+	}
 
-/**
-* print_number - Print an unsigned integer.
-* @number: The number to print.
-*
-* Return: The number of digits printed.
-*/
-int print_number(unsigned int number)
-{
-int count = count_digits(number);
-int i;
-
-for (i = count; i > 0; i--)
-{
-int digit = extract_digit(number, i);
-_putchar(digit + '0');
-}
-return (count);
-}
-
-/**
-* toInt - Print an integer as a string.
-* @value: The integer.
-*
-* Return: The number of digits printed.
-*/
-int toInt(va_list value)
-{
-int n = va_arg(value, int);
-unsigned int un_n;
-
-if (n < 0)
-{
-un_n = -n;
-_putchar('-');
-}
-else
-{
-un_n = n;
-}
-
-if (un_n == 0)
-{
-_putchar('0');
-return (1);
-}
-return (print_number(un_n));
+	for (int j = 0; j < num_digits; j++)
+	{
+		_putchar(buffer[j]);
+		i++;
+	}
+	return (i);
 }
