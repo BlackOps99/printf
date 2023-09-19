b6a133e1697cd52395e5f7afe3311765554b699a
Sarah <sarahaku12@gmail.com>
2023-09-19 11:34:24 +0300

    Message

--- print_int.c
+++ print_int.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
-/**
- * print_int - prints integer or decimal (they're the same in C).
- * @args: argument to print
- * Return: number of characters printed
- */
+/*
+* print_int - prints integer or decimal (as they're the same in C).
+* @args: argument to print
+* Return: number of characters printed
+*/
 int print_int(va_list args)
 {
 int n = va_arg(args, int);
-int num, last_digit = n % 10, digit;
-int count = 0;  // To count number of characters printed
+int num;
+int last_digit = n % 10;
+int digit;
+int count = 0;  /* To count number of characters printed */
 int exp = 1;
 
 if (last_digit < 0)
@@ -17,25 +19,24 @@ if (last_digit < 0)
 _putchar('-');
 count++;
 }
-n /= 10;  // Move to next digit
+n /= 10;  /* Move to next digit */
 
 if (n == 0)
 {
 _putchar('0' + last_digit);
-count++;
-return count;
+return (count + 1);
 }
 
-// Construct the number without the last digit
+/* Construct the number without the last digit */
 num = n;
 while (num)
 {
 count++;
-exp *= 10; 
+exp *= 10;
 num /= 10;
 }
 
-exp /= 10; // Adjust for last loop iteration
+exp /= 10; /* Adjust for last loop iteration */
 while (n)
 {
 digit = n / exp;
@@ -46,5 +47,5 @@ exp /= 10;
 _putchar('0' + last_digit);
 count++;
 
-return count;
+return (count);
 }
