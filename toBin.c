402d2b42522a718b5221aeb54630f05838a582d6
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-19 15:36:18 +0300

    test case

--- toBin.c
+++ toBin.c
@@ -6,33 +6,28 @@
  */
 int toBin(va_list value)
 {
-	unsigned int n, m, i;
+	unsigned int n, m, i, sum;
 	unsigned int a[32];
-	unsigned int sum = 0;
-	int Len = 0;
+	int count;
 
 	n = va_arg(value, unsigned int);
-	m = 2147483648;
+	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
-
 	for (i = 1; i < 32; i++)
 	{
 		m /= 2;
 		a[i] = (n / m) % 2;
 	}
-
-	for (i = 0; i < 32; i++)
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
 
-			_putchar(z);
-
-			Len++;
+			write(1, &z, 1);
+			count++;
 		}
 	}
-
-	return (Len);
+	return (count);
 }
