38504e55a39f7976d1064fd29849facde105ecf3
Sarah <sarahaku12@gmail.com>
2023-09-19 11:30:32 +0300

    Commit Message

--- print_int.c
+++ print_int.c
@@ -1,92 +1,50 @@
 #include "main.h"
-int print_number(unsigned int number);
-int count_digits(unsigned int num);
-int extract_digit(unsigned int num, int index);
+
 /**
- * toInt - Print an integer as a string
- * @value: The integer
- *
- * Return: The number of digits printed
+ * print_int - prints integer or decimal (they're the same in C).
+ * @args: argument to print
+ * Return: number of characters printed
  */
-int toInt(va_list value)
+int print_int(va_list args)
 {
-	int n = va_arg(value, int);
-	unsigned int un_n;
+int n = va_arg(args, int);
+int num, last_digit = n % 10, digit;
+int count = 0;  // To count number of characters printed
+int exp = 1;
 
-	if (n < 0)
-	{
-		un_n = -n;
-		_putchar('-');
-	}
-	else
-	{
-		un_n = n;
-	}
+if (last_digit < 0)
+{
+_putchar('-');
+count++;
+}
+n /= 10;  // Move to next digit
 
-	if (un_n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	return (print_number(un_n));
+if (n == 0)
+{
+_putchar('0' + last_digit);
+count++;
+return count;
 }
 
-/**
- * print_number - Prints an unsigned integer
- * @number: The unsigned integer
- *
- * Return: The number of digits printed
- */
-int print_number(unsigned int number)
+// Construct the number without the last digit
+num = n;
+while (num)
 {
-	int count = count_digits(number);
-
-	int i = 0;
-
-	for (i = count; i > 0; i--)
-	{
-		int digit = extract_digit(number, i);
-
-		_putchar(digit + '0');
-	}
-
-	return (count);
+count++;
+exp *= 10; 
+num /= 10;
 }
 
-/**
- * count_digits - Counts the number of digits in a number
- * @num: The number
- *
- * Return: The number of digits
- */
-int count_digits(unsigned int num)
+exp /= 10; // Adjust for last loop iteration
+while (n)
 {
-	int digits = 0;
-
-	while (num)
-	{
-		digits++;
-		num /= 10;
-	}
-	return (digits);
+digit = n / exp;
+_putchar('0' + digit);
+n -= digit * exp;
+exp /= 10;
 }
+_putchar('0' + last_digit);
+count++;
 
-/**
- * extract_digit - Extracts the desired digit from a number
- * @num: The number
- * @index: The index (from the right, starting with 1)
- *
- * Return: The desired digit
- */
-int extract_digit(unsigned int num, int index)
-{
-	int i = 0;
-
-	for (i = 0; i < index - 1; i++)
-	{
-		num /= 10;
-	}
-
-	return (num % 10);
+return count;
 }
