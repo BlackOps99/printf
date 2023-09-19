659a306268bada964ec01ce9dd797425e95a8908
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-19 15:37:10 +0300

    test case 5

--- toBin.c
+++ toBin.c
@@ -4,30 +4,44 @@
  * @value: unsigned integer
  * Return: binary
  */
+int countBits(unsigned int num);
+
 int toBin(va_list value)
 {
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
+	unsigned int result = va_arg(value, unsigned int);
 
-	n = va_arg(value, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
+	int i;
+
+	int leadingZero = 1;
+
+	for (i = 31; i >= 0; i--)
 	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
+		char bit = ((result >> i) & 1) + '0';
+		if (bit == '1')
 		{
-			char z = '0' + a[i];
-
-			write(1, &z, 1);
-			count++;
+			leadingZero = 0;
+		}
+		if (!leadingZero)
+		{
+			_putchar(bit);
 		}
 	}
-	return (count);
+
+	return (countBits(result));
+}
+
+/**
+ * countBits - calculate the number of binary
+ * @num: unsigned integer
+ * Return: number of binary
+ */
+int countBits(unsigned int num)
+{
+	int count = 0;
+	while (num > 0)
+	{
+		count++;
+		num >>= 1;
+	}
+	return count;
 }
