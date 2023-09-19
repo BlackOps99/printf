ddf448dd34fc46280f0b3aa497958eb3095378c0
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-19 15:48:04 +0300

    test case 6

--- toBin.c
+++ toBin.c
@@ -1,47 +1,44 @@
 #include "main.h"
+char *convert(unsigned long int num, int base, int lowercase);
 /**
  * toBin - unsigned int to converted it to binary
  * @value: unsigned integer
  * Return: binary
  */
-int countBits(unsigned int num);
-
 int toBin(va_list value)
 {
-	unsigned int result = va_arg(value, unsigned int);
+	unsigned int num = va_arg(value, unsigned int);
+	char *str = convert(num, 2, 0);
 
-	int i;
+	int len;
 
-	int leadingZero = 1;
+	for (len = 0; str[len] != '\0'; len++)
+		_putchar(str[len]);
 
-	for (i = 31; i >= 0; i--)
-	{
-		char bit = ((result >> i) & 1) + '0';
-		if (bit == '1')
-		{
-			leadingZero = 0;
-		}
-		if (!leadingZero)
-		{
-			_putchar(bit);
-		}
-	}
-
-	return (countBits(result));
+	return (len);
 }
-
 /**
- * countBits - calculate the number of binary
- * @num: unsigned integer
- * Return: number of binary
+ * convert - converts number and base into string
+ * @num: input number
+ * @base: input base
+ * @lowercase: flag if hexa values need to be lowercase
+ * Return: result string
  */
-int countBits(unsigned int num)
+char *convert(unsigned long int num, int base, int lowercase)
 {
-	int count = 0;
-	while (num > 0)
-	{
-		count++;
-		num >>= 1;
-	}
-	return count;
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = (lowercase)
+		  ? "0123456789abcdef"
+		  : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
 }
