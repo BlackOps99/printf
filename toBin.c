e8988e557537e8ad891201ea2c166bf9c504abae
Sarah <sarahaku12@gmail.com>
2023-09-20 00:36:03 +0300

    Fix Mostafas bin 1

--- toBin.c
+++ toBin.c
@@ -1,51 +1,44 @@
 #include "main.h"
-
-int countBits(unsigned int num);
 /**
  * toBin - unsigned int to converted it to binary
- * @value: unsigned integer
+ * @val: unsigned integer
  * Return: binary
  */
-int toBin(va_list value)
+int toBin(va_list val)
 {
-	unsigned int result = va_arg(value, unsigned int);
+	unsigned int length, power_of_two, index, digit, value, number;
+	int character_count = 0;
 
-	int i;
+	number = va_arg(val, unsigned int);
 
-	int leadingZero = 1;
-
-	for (i = 31; i >= 0; i--)
+	if (number == 0)
 	{
-		char bit = ((result >> i) & 1) + '0';
-
-		if (bit == '1')
-		{
-			leadingZero = 0;
-		}
-
-		if (!leadingZero)
-		{
-			_putchar(bit);
-		}
+		_putchar('0');
+		return (1);
 	}
 
-	return (countBits(result));
-}
+	value = number;
+	length = 0;
 
-/**
- * countBits - calculate the number of binary
- * @num: unsigned integer
- * Return: number of binary
- */
-int countBits(unsigned int num)
-{
-	int count = 0;
-
-	while (num > 0)
+	while (value != 0)
 	{
-		count++;
-		num >>= 1;
+		value /= 2;
+		length++;
 	}
 
-	return (count);
+	power_of_two = 1;
+
+	for (index = 1; index <= length - 1; index++)
+		power_of_two *= 2;
+
+	for (index = 1; index <= length; index++)
+	{
+		digit = number / power_of_two;
+		_putchar(digit + '0');
+		character_count++;
+		number -= digit * power_of_two;
+		power_of_two /= 2;
+	}
+
+	return (character_count);
 }
